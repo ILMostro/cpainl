e4678456b59fbbacd6f5313a4b22c1b8fc785f0e
ILMostro <ilmostro7@gmail.com>
2016-03-28 18:07:45 -0500

    telnet/ssh in firefox

---
+++ script.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+#
+use strict;
+use warnings;
+use IO::File;
+
+# my @matches = $text =~ \d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/g;
+
+
+# while (<>) {
+  # print $text
+# }
+
+# open FH, "</home/wiredtree/devel/test.test";
+# $line = <FH>;
+# print $line;
+
+# $fh = IO::File->new();
+# if ($fh->open("< /home/wiredtree/devel/test.test")) {
+  # print <$fh>;
+  # $fh->close;
+
+# if ( <> =~ /(\d+)/ ) {
+  # print "Numbers: $1";
+# }
+
+# open(DATA, "<test.test") or die "Couldn't open file test.test, $!";
+
+# while(<DATA>){
+  # print "$_";
+# }
+
+open(DATA, "
